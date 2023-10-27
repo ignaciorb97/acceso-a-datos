74fe3abdc96fd4a7087312b0ffa9e5ce7ef397e8
Ignacio Rodríguez Blanco <ignaciorodriguezblanco97@gmail.com>
2023-10-27 14:18:17 +0200

    terminado

--- DAOprimero/src/main/java/com/ignaciorodriguez/daoprimero/DeleteFrom.java
+++ DAOprimero/src/main/java/com/ignaciorodriguez/daoprimero/DeleteFrom.java
@@ -5,25 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InsertarDatos {
+public class DeleteFrom {
 
 	public static void main(String[] args) {
 		Connection conn=null;
 		Statement stmt=null;
-		String sql= "INSERT INTO clientes (id, nif, nombre, edad)"
-				+"VALUES (1, '11111', 'Alvaro', '29')";
-
+		String sql= "DELETE FROM clientes WHERE edad ='1'";
 		try {
 			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa","root","");
 			stmt=conn.createStatement();
 			stmt.execute(sql);
-			System.out.println("Datos insertados en la tabla \"clientes\" correctamente");
+			System.out.println("cliente borrado");
 			conn.close();
 			stmt.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	
 	}
 
 }
--- DAOprimero/src/main/java/com/ignaciorodriguez/daoprimero/MostrarDatos.java
+++ DAOprimero/src/main/java/com/ignaciorodriguez/daoprimero/MostrarDatos.java
@@ -2,6 +2,7 @@ package com.ignaciorodriguez.daoprimero;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,18 +11,24 @@ public class MostrarDatos {
 	public static void main(String[] args) {
 		Connection conn=null;
 		Statement stmt=null;
-		String sql= "INSERT INTO clientes (id, nif, nombre, edad)"
-				+"VALUES (1, '11111', 'Alvaro', '29')";
+		String sql= "SELECT * FROM clientes";
 
 		try {
 			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa","root","");
 			stmt=conn.createStatement();
 			stmt.execute(sql);
-			System.out.println("Datos insertados en la tabla \"clientes\" correctamente");
+			ResultSet rs=stmt.executeQuery(sql);
+			while (rs.next()) {
+				int id=rs.getInt("id");
+				String nif=rs.getString("nif");
+				String nombre=rs.getString("nombre");
+				String edad=rs.getString("edad");
+				System.out.println("id: "+id+", nif: "+nif+", nombre: "+nombre+", edad: "+edad);
+			}
 			conn.close();
 			stmt.close();
+			rs.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
